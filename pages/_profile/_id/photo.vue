696a4bd915bd707e82846c15c7ab989713aabe0a
AGD <agdholo@gmail.com>
2019-08-19 17:23:19 +0800

    feat: create photo in profile page

--- pages/_profile/_id/photo.vue
+++ pages/_profile/_id/photo.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>
-    photo
-  </div>
+  <v-timeline dense>
+    <v-timeline-item v-for="i in years" :key="i" small>
+      <v-row justify="start" align="center">
+        <v-col sm="12" md="1" cols="12">
+          <span class="headline font-weight-bold">
+            {{ i }}
+          </span>
+        </v-col>
+        <v-col sm="12" md="11" cols="12">
+          <v-row>
+            <v-col
+              v-for="(item, k) in photos"
+              :key="k"
+              cols="6"
+              md="4"
+              sm="6"
+              lg="3"
+              xl="2"
+            >
+              <v-card hover>
+                <v-img :src="item" :aspect-ratio="16 / 9"></v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </v-col>
+      </v-row>
+    </v-timeline-item>
+  </v-timeline>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    photos: [
+      'https://pbs.twimg.com/media/EBPDVUzU8AA-Ycu?format=jpg&name=large',
+      'https://pbs.twimg.com/media/EA6bZJBXYAA4M8w?format=jpg&name=4096x4096',
+      'https://pbs.twimg.com/media/EA2Sr1GU4AA3KPa?format=jpg&name=4096x4096',
+      'https://pbs.twimg.com/media/EAfBHYfU0AATrVN?format=jpg&name=medium',
+      'https://pbs.twimg.com/media/EAP6X-9U4AArcMP?format=jpg&name=4096x4096',
+      'https://pbs.twimg.com/media/D27_AItUcAAHku_?format=jpg&name=large'
+    ],
+    years: [2019, 2018, 2017, 2016, 2015]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.v-timeline {
+  padding: 0;
+}
+</style>
