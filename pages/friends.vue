ca706c62d14e21873ad98e8abf13e097535bedc4
AGD <agdholo@gmail.com>
2019-12-18 12:31:15 +0800

    feat: add avatar in frends page

--- pages/friends.vue
+++ pages/friends.vue
@@ -2,8 +2,12 @@
   <v-row>
     <v-col v-for="i in 30" :key="i" xs="12" cols="6" md="4" xl="3">
       <v-card outlined hover to="/profile/Elon Musk">
-        <v-row align="center" justify="start" class="fill-height pl-5">
-          <v-col>
+        <v-row align="center" justify="start" no-gutters>
+          <v-flex shrink>
+            <v-img :src="'https://cdn.vuetifyjs.com/images/lists/1.jpg'">
+            </v-img>
+          </v-flex>
+          <v-col class="pl-5">
             <span class="title">
               Elon Musk
             </span>
